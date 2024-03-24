c021caa15452f4ebd183cc74c41d69626e2bb05d
Denis Oliveira <denis.bass.oliveira@gmail.com>
2024-03-24 12:41:15 -0300

    Prova programação scripts

--- index.js
+++ index.js
@@ -33,6 +33,10 @@ createApp({
             }
         },
         lidarNumero(botao){
+            //para evitar que o usuário continue realizando operações com sujeira das variáveis\
+            //esta rotina, primeiro, verifica se uma nova operação está sendo iniciada\
+            //caso tenha sido clicado em "=" ou "AC", concatenando os valores da String contida no display\
+            //a cada clique nos números, evitando que a string inicie em 0
             if (this.display == 0 || this.novaOperacao){
                 this.display = botao;
                 this.numeroAtual = this.display;
@@ -44,6 +48,8 @@ createApp({
             }
         },
         lidarOperador(botao){
+            //seta o valor atual em uma segunda variável para futura realização dos cálculos\
+            //caso seja uma nova operação, essa rotina retorna sem nenhuma ação, para evitar cálculos com sujeiras das variáveis
             if(!this.novaOperacao){
                 this.numeroAnterior = this.numeroAtual;
                 this.operador = botao;
@@ -52,10 +58,13 @@ createApp({
             else return;
         },
         lidarDecimal(){
+            //apenas adiciona o "." caso já nao contenha antes na string\
+            //para evitar ter mais de 1 caracter "." na string
             if (!this.display.includes('.')) this.display = this.display+'.';
             this.numeroAtual = this.display;
         },
         lidarIgual(){
+            //realiza os cálculos dependendo do operador selecionado anteriormente
             var resultado = 0;
             switch (this.operador){
                 case "*":
@@ -71,15 +80,23 @@ createApp({
                     resultado = parseFloat(this.numeroAnterior) / parseFloat(this.numeroAtual);
                     break;
                 default:
-                    return;              
+                    return;//caso o usuário clique em "=" sem antes selecionar um operador\
+                    //esta rotina retorna sem nenhuma ação
             }
             this.display = resultado;
+
+            //após realizar os cálculos e exibir no display\
+            //as variáveis são zeradas e novaOperação = true\
+            //indicando que esta operação já finalizou e uma nova está sendo iniciada\
+            //recomeçando assim os cálculos desde o início novamente
             this.numeroAtual = null;
             this.numeroAnterior = null;
             this.operador = null;
             this.novaOperacao = true;
         },
         lidarClear(){
+            //zera todas as variáveis e exibe 0 no display,\
+            //iniciando os cálculos desde o início novamente
             this.display = 0;
             this.numeroAtual = null;
             this.numeroAnterior = null;
